21148c5808925515ec6a0096d5e91264884568c6
h25tykto <h25tykto@du.se>
2026-03-20 14:34:47 +0100

    Fixat ögonfärg och ToString()

--- Laboration_4/Person.cs
+++ Laboration_4/Person.cs
@@ -7,13 +7,20 @@ public class Person
     public DateTime DateOfBirth { get; set; } 
     public string FirstName { get; set; } 
     public string LastName { get; set; } 
+    public string EyeColor { get; set; }
 
-    public Person(Hair hair, Gender gender, DateTime dateOfBirth, string firstName, string lastName)
+    public Person(Hair hair, Gender gender, DateTime dateOfBirth, string firstName, string lastName, string eyeColor)
     {
         Hair = hair;
         Gender = gender;
         DateOfBirth = dateOfBirth;
         FirstName = firstName;
         LastName = lastName;
+        EyeColor = eyeColor;
+    }
+
+    public override string ToString()
+    {
+        return $"Namn: {FirstName} {LastName} | Kön: {Gender} | Född: {DateOfBirth} | Hår: {Hair.Lenght}, {Hair.Color} | Ögonfärg: {EyeColor}";
     }
 }
